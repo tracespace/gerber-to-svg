ef0e411eeb5db912304469cd52c49dd1dd169051
Mike Cousins <mike@cousins.io>
2014-08-11 09:27:43 -0400

    licensing stuff

--- packages/gerber-to-svg/dist/gerber-to-svg.js
+++ packages/gerber-to-svg/dist/gerber-to-svg.js
@@ -1,4 +1,10 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.gerberToSvg=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+/*
+@license copyright 2014 by mike cousins <mike@cousins.io> (http://cousins.io)
+shared under the terms of the MIT license
+view source at http://github.com/mcous/gerber-to-svg
+ */
 var Plotter, builder, gerberToSvg;
 
 builder = require('./obj-to-xml');
