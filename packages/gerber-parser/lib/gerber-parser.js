70d4f9a411b3e01d2a3484c9dcc416dcfb1fdf94
Mike Cousins <mike@cousins.io>
2015-08-24 17:11:19 -0400

    stub out initial gerber parser class

--- packages/gerber-parser/lib/gerber-parser.js
+++ packages/gerber-parser/lib/gerber-parser.js
@@ -2,38 +2,41 @@
 'use strict'
 
 const Transform = require('stream').Transform
+const util = require('util')
 
-class Parser extends Transform {
-  // constructor(opts = {}) {
-  //   this.format = {
-  //     zero: opts.zero || null,
-  //     places: opts.places || null
-  //   }
-  // }
+util.inherits(GerberParser, Transform)
+function GerberParser(opts) {
+  if (opts == null) {
+    opts = {}
+  }
+  this.format = {
+    zero: opts.zero || null,
+    places: opts.places || null
+  }
 
-  //   # make sure places was set correctly
-  //   if @format.places?
-  //     if @format.places.length isnt 2 or
-  //     typeof @format.places[0] isnt 'number' or
-  //     typeof @format.places[1] isnt 'number'
-  //       throw new Error 'parser places format must be an array of two numbers'
-  //
-  //   # make sure zero was set correctly
-  //   if @format.zero? and @format.zero isnt 'L' and @format.zero isnt 'T'
-  //     throw new Error "parser zero format must be either 'L' or 'T'"
-  //
-  //   super {
-  //     readableObjectMode: true
-  //     writableObjectMode: true
-  //   }
-  //
-  // _transform: (chunk, encoding, done) ->
-  //   if chunk.block?
-  //     @parseBlock chunk.block, chunk.line, done
-  //   else if chunk.param?
-  //     @parseParam chunk.param, chunk.line, done
-  //   else
-  //     done()
+  // make sure places was set correctly
+  if (this.format.places) {
+    if ((this.format.places.length !== 2) ||
+    (!Number.isFinite(this.format.places[0])) ||
+    (!Number.isFinite(this.format.places[1]))) {
+      throw new Error('parser places format must be an array of two numbers')
+    }
+  }
+
+  // make sure zero was set correctly
+  if (this.format.zero && (this.format.zero !== 'L') && (this.format.zero !== 'T')) {
+    throw new Error("parser zero format must be either 'L' or 'T'")
+  }
+
+  Transform.call()
 }
 
-module.exports = Parser
+// _transform: (chunk, encoding, done) ->
+//   if chunk.block?
+//     @parseBlock chunk.block, chunk.line, done
+//   else if chunk.param?
+//     @parseParam chunk.param, chunk.line, done
+//   else
+//     done()
+
+module.exports = GerberParser
